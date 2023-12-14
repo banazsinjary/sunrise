18a111035caf3f28de45e55f1f28a47b55319fdf
banaz <banazsinjary@gmail.com>
2023-12-13 19:10:43 -0800

    content dump

--- src/App.tsx
+++ src/App.tsx
@@ -6,51 +6,61 @@ function App() {
   return (
     <>
       <div id='landing'>
-       <nav></nav>
-       <h1></h1>
-       <h2></h2>
+       <nav>
+        <ul>
+          <li><a href="#today">Today</a></li>
+          <li><a href="#coach">Coach</a></li>
+          <li><a href="#later">Later</a></li>
+          <li><a href="#quote">Reflect</a></li>
+        </ul>
+       </nav>
+       <h1>Simple to-do lists are powerful.</h1>
+       <h2>Long to-do lists kill motivation</h2>
       </div>
 
       <div id='today'>
-        <p></p>
-        <h1></h1>
-        <p></p>
+        <p>Today</p>
+        <h1>Focus on What Matters</h1>
+        <p>Sunrise is all about getting the important things done with minimal stress.</p>
+        <p>You can only have four tasks to get done at any time, helping you to focus on what's important.</p>
         <img></img>
       </div>
 
       <div id='coach'>
-        <p></p>
-        <h1></h1>
-        <p></p>
+        <p>Coach</p>
+        <h1>Your personal productivity coach</h1>
+        <p>Powered by AI and available at all times, it has been trained on the latest productivity research and includes everything that makes Daily Four great.</p>
         <img></img>
       </div>
 
       <div id='later'>
-        <p></p>
-        <h1></h1>
-        <p></p>
+        <p>Later</p>
+        <h1>Over 40% of your tasks are never completed</h1>
+        <p>With Later, you capture tasks you don't need to get done today.</p>
+        <p>As priorities change, tasks naturally fade. By reviewing daily, you can maintain a workload that aligns with your goals.</p>
         <img></img>
       </div>
 
       <div id='quote'>
-        <h1></h1>
+        <h1>Designed as a quiet place to do your best work.</h1>
       </div>
 
       <div id='reflect'>
-        <p></p>
-        <h1></h1>
-        <p></p>
+        <p>Reflect</p>
+        <h1>You get more done than you think</h1>
+        <p>Reflect is a non-judgmental view of your productivity. There are no streaks to complete, and your completed tasks are grouped by week and month.</p>
+        <p>Reflect regularly or simply whenever you need a dose of perspective.</p>
         <img></img>
       </div>
 
       <div>
-        <h1></h1>
+        <h1>Daily Four is in private beta. Request early access.</h1>
         <input type='email' id="email"/>
-        <button id='submit'></button>
+        <button id='submit'>SUBMIT</button>
       </div>
       
       <footer>
-        <p></p>
+        <p>2023 Coded with 🤎 by Banaz</p>
       </footer>
     </>
   )
